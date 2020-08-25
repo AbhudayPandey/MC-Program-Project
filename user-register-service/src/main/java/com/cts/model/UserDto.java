a430317f094030dc9d38ba598d2dfff9375f45ef
AbhudayPandey <60208836+AbhudayPandey@users.noreply.github.com>
2020-08-25 17:12:35 +0530

    Add files via upload

---
+++ user-register-service/src/main/java/com/cts/model/UserDto.java
@@ -0,0 +1,67 @@
+package com.cts.model;
+
+
+public class UserDto {
+	
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String pan;
+    
+    private String password;
+    
+    private String contact; 
+    
+    private String mailId;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPan() {
+		return pan;
+	}
+
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public String getMailId() {
+		return mailId;
+	}
+
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}   
+
+}
